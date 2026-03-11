10ca878e8bc99f50507faca3ed2a2a8e00776810
Pham Le Gia Han <giahanphamle.se@gmail.com>
2026-03-12 00:00:55 +0700

    fix bug main menu

--- Assets/MainMenu/Prefabs/MainMenu.cs
+++ Assets/MainMenu/Prefabs/MainMenu.cs
@@ -3,12 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("Loader"); 
-    }
-    public void QuitGame()
-    {
-        Application.Quit();
+        SceneManager.LoadScene("Demo");
     }
 }
